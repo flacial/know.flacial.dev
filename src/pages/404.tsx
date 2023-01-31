ac415415dc38a3e60df1e4441d025e966d94ffcc
Flacial <ruthemiumwithdiamond@protonmail.com>
2023-01-31 04:33:52 +0400

    Update 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,16 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from 'react'
+import { Link } from 'gatsby'
+import { Button, Typography } from '@mui/material'
 
 // styles
 const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  color: '#232129',
+  padding: '96px',
+  fontFamily: '-apple-system, Roboto, sans-serif, serif',
 }
 const headingStyles = {
   marginTop: 0,
-  marginBottom: 64,
+  marginBottom: 36,
   maxWidth: 320,
 }
 
@@ -17,10 +18,10 @@ const paragraphStyles = {
   marginBottom: 48,
 }
 const codeStyles = {
-  color: "#8A6534",
+  color: '#8A6534',
   padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
+  backgroundColor: '#FFF4DB',
+  fontSize: '1rem',
   borderRadius: 4,
 }
 
@@ -29,15 +30,17 @@ const NotFoundPage = () => {
   return (
     <main style={pageStyles}>
       <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
+      <Typography variant="h4" style={headingStyles}>
+        Page not found
+      </Typography>
       <p style={paragraphStyles}>
-        Sorry{" "}
+        Sorry{' '}
         <span role="img" aria-label="Pensive emoji">
           😔
-        </span>{" "}
+        </span>{' '}
         we couldn’t find what you were looking for.
         <br />
-        {process.env.NODE_ENV === "development" ? (
+        {process.env.NODE_ENV === 'development' ? (
           <>
             <br />
             Try creating a page in <code style={codeStyles}>src/pages/</code>.
@@ -45,7 +48,11 @@ const NotFoundPage = () => {
           </>
         ) : null}
         <br />
-        <Link to="/">Go home</Link>.
+        <Link to="/" style={{ textDecoration: 'none' }}>
+          <Button variant="text" color="info">
+            Go home
+          </Button>
+        </Link>
       </p>
     </main>
   )
