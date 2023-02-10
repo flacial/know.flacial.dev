b388b3abb3bb41e595eaef9fd10f7e18147b6e81
Flacial <ruthemiumwithdiamond@protonmail.com>
2023-02-10 16:09:07 +0400

    refactor: Improve code readability

--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -5,10 +5,35 @@ import Link from '@mui/material/Link'
 
 const Breadcrumb = ({ location }: { location?: string }) => {
   const Home = (
-    <Link underline="hover" key={0} color="text.secondary" href={'/'}>
+    <Link underline="hover" key={0} color="text.secondary" href="/">
       Home
     </Link>
   )
+
+  const mapPathToLink = (e: string, i: number, arr: string[]) => {
+    const locationArr = (location || '').split('/')
+    const index = locationArr.indexOf(e)
+    const value = locationArr.filter((_, i) => i <= index)
+
+    if (i === arr.length - 1) {
+      return (
+        <Typography key={i + 1} color="text.primary">
+          {e}
+        </Typography>
+      )
+    }
+
+    return (
+      <Link
+        underline="hover"
+        key={i + 1}
+        color="text.secondary"
+        href={value.join('/')}
+      >
+        {e}
+      </Link>
+    )
+  }
 
   const breadcrumbs =
     location &&
@@ -17,30 +42,7 @@ const Breadcrumb = ({ location }: { location?: string }) => {
       location
         .split('/')
         .filter((e) => e !== '')
-        .map((e, i, arr) => {
-          const locationArr = location.split('/')
-          const index = locationArr.indexOf(e)
-          const value = locationArr.filter((_, i) => i <= index)
-
-          if (i === arr.length - 1) {
-            return (
-              <Typography key={i + 1} color="text.primary">
-                {e}
-              </Typography>
-            )
-          }
-
-          return (
-            <Link
-              underline="hover"
-              key={i + 1}
-              color="text.secondary"
-              href={value.join('/')}
-            >
-              {e}
-            </Link>
-          )
-        })
+        .map(mapPathToLink)
     )
 
   return location ? <Breadcrumbs>{breadcrumbs}</Breadcrumbs> : <></>
